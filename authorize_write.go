0c4254460da5c6e7911c8a2f11a1334f0daa30a1
James Elliott <james-d-elliott@users.noreply.github.com>
2024-03-23 22:50:55 +1100

    refactor: misc

--- authorize_write.go
+++ authorize_write.go
@@ -6,12 +6,10 @@ package oauth2
 import (
 	"context"
 	"net/http"
-
-	"authelia.com/provider/oauth2/internal/consts"
 )
 
 func (f *Fosite) WriteAuthorizeResponse(ctx context.Context, rw http.ResponseWriter, requester AuthorizeRequester, responder AuthorizeResponder) {
-	for _, handler := range f.ResponseModeHandlers(ctx) {
+	for _, handler := range f.Config.GetResponseModeHandlers(ctx) {
 		if handler.ResponseModes().Has(requester.GetResponseMode()) {
 			handler.WriteAuthorizeResponse(ctx, rw, requester, responder)
 
@@ -20,18 +18,4 @@ func (f *Fosite) WriteAuthorizeResponse(ctx context.Context, rw http.ResponseWri
 	}
 
 	f.handleWriteAuthorizeErrorJSON(ctx, rw, ErrServerError.WithHint("The Authorization Server was unable to process the requested Response Mode."))
-}
-
-// https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.1
-// When a decision is established, the authorization server directs the
-// user-agent to the provided client redirection URI using an HTTP
-// redirection response, or by other means available to it via the
-// user-agent.
-//
-// TODO: Investigate this unused.
-//
-//nolint:unused
-func sendRedirect(url string, rw http.ResponseWriter) {
-	rw.Header().Set(consts.HeaderLocation, url)
-	rw.WriteHeader(http.StatusSeeOther)
 }

533774387646c721077afe7c1168ff1dad7d9e86
James Elliott <james-d-elliott@users.noreply.github.com>
2024-03-29 20:44:02 +1100

    docs: misc docs and error debug information changes

--- client.go
+++ client.go
@@ -116,9 +116,17 @@ type RevokeFlowRevokeRefreshTokensExplicitClient interface {
 
 // JARMClient is a client which supports JARM.
 type JARMClient interface {
+	// GetAuthorizationSignedResponseKeyID returns the 'authorization_signed_response_alg' key identifier instead of
+	// using the alg.
 	GetAuthorizationSignedResponseKeyID() (kid string)
+
+	// GetAuthorizationSignedResponseAlg returns the 'authorization_signed_response_alg' value.
 	GetAuthorizationSignedResponseAlg() (alg string)
+
+	// GetAuthorizationEncryptedResponseAlg returns the 'authorization_encrypted_response_alg' value.
 	GetAuthorizationEncryptedResponseAlg() (alg string)
+
+	// GetAuthorizationEncryptedResponseEncryptionAlg returns the 'authorization_encrypted_response_enc' value.
 	GetAuthorizationEncryptedResponseEncryptionAlg() (alg string)
 
 	Client
--- token/jarm/types.go
+++ token/jarm/types.go
@@ -14,10 +14,20 @@ type Configurator interface {
 }
 
 type Client interface {
+	// GetID returns the client ID.
 	GetID() string
+
+	// GetAuthorizationSignedResponseKeyID returns the 'authorization_signed_response_alg' key identifier instead of
+	// using the alg.
 	GetAuthorizationSignedResponseKeyID() (kid string)
+
+	// GetAuthorizationSignedResponseAlg returns the 'authorization_signed_response_alg' value.
 	GetAuthorizationSignedResponseAlg() (alg string)
+
+	// GetAuthorizationEncryptedResponseAlg returns the 'authorization_encrypted_response_alg' value.
 	GetAuthorizationEncryptedResponseAlg() (alg string)
+
+	// GetAuthorizationEncryptedResponseEncryptionAlg returns the 'authorization_encrypted_response_enc' value.
 	GetAuthorizationEncryptedResponseEncryptionAlg() (alg string)
 }
 

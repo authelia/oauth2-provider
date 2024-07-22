a4cfffb769668cac6c86cec032118e02a752edd1
James Elliott <james-d-elliott@users.noreply.github.com>
2024-07-22 22:03:09 +1000

    refactor: additional client methods for uniformity

--- token/jarm/types.go
+++ token/jarm/types.go
@@ -14,10 +14,37 @@ type Configurator interface {
 }
 
 type Client interface {
-	GetID() string
+	// GetID returns the client ID.
+	GetID() (id string)
+
+	// GetAuthorizationSignedResponseKeyID returns the specific key identifier used to satisfy JWS requirements of the
+	// JWT-secured Authorization Response Method (JARM) specifications. If unspecified the other available parameters
+	// will be utilized to select an appropriate key.
 	GetAuthorizationSignedResponseKeyID() (kid string)
+
+	// GetAuthorizationSignedResponseAlg is equivalent to the 'authorization_signed_response_alg' client metadata
+	// value which determines the JWS [RFC7515] alg algorithm JWA [RFC7518] REQUIRED for signing authorization
+	// responses. If this is specified, the response will be signed using JWS and the configured algorithm. The
+	// algorithm none is not allowed. The default, if omitted, is RS256.
 	GetAuthorizationSignedResponseAlg() (alg string)
+
+	// GetAuthorizationEncryptedResponseKeyID returns the specific key identifier used to satisfy JWE requirements of
+	// the JWT-secured Authorization Response Method (JARM) specifications. If unspecified the other available parameters will be
+	// utilized to select an appropriate key.
+	GetAuthorizationEncryptedResponseKeyID() (kid string)
+
+	// GetAuthorizationEncryptedResponseAlg is equivalent to the 'authorization_encrypted_response_alg' client metadata
+	// value which determines the JWE [RFC7516] alg algorithm JWA [RFC7518] REQUIRED for encrypting authorization
+	// responses. If both signing and encryption are requested, the response will be signed then encrypted, with the
+	// result being a Nested JWT, as defined in JWT [RFC7519]. The default, if omitted, is that no encryption is
+	// performed.
 	GetAuthorizationEncryptedResponseAlg() (alg string)
+
+	// GetAuthorizationEncryptedResponseEncryptionAlg is equivalent to the 'authorization_encrypted_response_enc' client
+	// metadata value which determines the JWE [RFC7516] enc algorithm JWA [RFC7518] REQUIRED for encrypting
+	// authorization responses. If authorization_encrypted_response_alg is specified, the default for this value is
+	// A128CBC-HS256. When authorization_encrypted_response_enc is included, authorization_encrypted_response_alg MUST
+	// also be provided.
 	GetAuthorizationEncryptedResponseEncryptionAlg() (alg string)
 }
 
